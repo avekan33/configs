c85de84224e45a4f8b364c8909a781f6448abe04
avekan33 <avekan33@gmail.com>
2019-10-04 00:27:51 -0700

    need to merge pdfs lol

---
+++ pdfmerge.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+outputfile=$1
+shift
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -r150 -sOutputFile=$outputfile "$@"
